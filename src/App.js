bf980431e89b256ce513a1463e1d4d59d67a70c1
Abhinav Behal <abhinav.behal@hotmail.com>
2018-12-06 19:49:11 +1300

    Added a transition when submitting the username

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { value: '', submitted: false };
   }
 
   usernameSubmitted = (e) => {
+    this.setState({ submitted: true });
     console.log(this.state.value);
     e.preventDefault();
   }
 
   usernameChanged = (e) => {
     this.setState({ value: e.target.value });
+    // todo: decide on what to do when there's no text
+    if (e.target.value === '') {
+      this.setState({ submitted: false });
+    }
   }
 
   render() {
     return (
-      <div className="app">
+      <div className={`app ${this.state.submitted ? 'move-up' : ''}`}>
         <h1>genius.fm</h1>
         <form className="user-search" onSubmit={this.usernameSubmitted}>
           <input type="text" placeholder="your last.fm username" onChange={this.usernameChanged}></input>
