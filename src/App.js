c4c02388f51c5f7e272ae8560064e1319a1cdd77
Abhinav Behal <abhinav.behal@hotmail.com>
2018-12-06 19:19:53 +1300

    Basic layout of page and logic for obtaining the entered username

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  usernameSubmitted = (e) => {
+    console.log(this.state.value);
+    e.preventDefault();
+  }
+
+  usernameChanged = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <h1>genius.fm</h1>
+        <form className="user-search" onSubmit={this.usernameSubmitted}>
+          <input type="text" placeholder="your last.fm username" onChange={this.usernameChanged}></input>
+        </form>
       </div>
     );
   }
