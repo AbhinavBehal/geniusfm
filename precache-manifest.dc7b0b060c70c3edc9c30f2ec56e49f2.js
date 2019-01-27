b9a27d1e234a92c9f0402662bc20f72a1984dbc1
Abhinav Behal <abhinav.behal@hotmail.com>
2019-01-28 12:46:24 +1300

    Updates

---
+++ precache-manifest.dc7b0b060c70c3edc9c30f2ec56e49f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbf552baf8ffb8a94e88",
+    "url": "/geniusfm/static/js/runtime~main.dbf552ba.js"
+  },
+  {
+    "revision": "7d3cf1f0338cc198988c",
+    "url": "/geniusfm/static/js/main.7d3cf1f0.chunk.js"
+  },
+  {
+    "revision": "57914514eb2035618fbc",
+    "url": "/geniusfm/static/js/1.57914514.chunk.js"
+  },
+  {
+    "revision": "7d3cf1f0338cc198988c",
+    "url": "/geniusfm/static/css/main.9b9ed6e9.chunk.css"
+  },
+  {
+    "revision": "20123767393746875a60c9fc8afaab6d",
+    "url": "/geniusfm/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/geniusfm/precache-manifest.b9616a11ec0a84bd1a4126fe292a0592.js"
+  "/geniusfm/precache-manifest.dc7b0b060c70c3edc9c30f2ec56e49f2.js"
 );
 
 workbox.clientsClaim();
